b16f6e3041f938c5c9e50b18f2cce142d5f61eb7
Lucía A. <u20211a452@upc.edu.pe>
2024-11-16 16:52:22 -0500

    Initial commit

---
+++ src/main/java/com/socialtech/eppeye/EppeyeApplication.java
@@ -0,0 +1,13 @@
+package com.socialtech.eppeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EppeyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EppeyeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/socialtech/eppeye/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.socialtech.eppeye;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EppeyeApplication.class);
+	}
+
+}
